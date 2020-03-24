f976d17cad0c29af7cdd5e39b58813343f5ad975
TibitFromTaiwan <jim870718@gmail.com>
2020-03-25 03:58:30 +0800

    .

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+var id = 0
 class dot {
     constructor(type, position) {
         this.type = type
         this.position = position
+        this.id = id
+        $('.canvas').append('<div id="point-' + this.id + '"></div>')
         this.draw()
+        id += 1
     }
-    get position() {
-        return this.position
-    }
+
     draw() {
+        $('#point-' + this.id).css('position', 'absolute')
+            .css('width', 4)
+            .css('height', '4px')
+            .css('border', '1px black solid')
+            .css('left', this.position[0] - 2 + 'px')
+            .css('top', this.position[1] - 2 + 'px')
     }
-
-}
-window.onload = () => {
-
 }
 
-$('.canvas').click(function (e) {
-    e.preventDefault();
+dot_list = []
+$('.canvas').mousedown(function (e) {
+    dot_list.push(new dot('point', [e.pageX, e.pageY]))
 
 });
