f9754d322aa975f7d146bb9b999c50bc8d83256b
Li Chun Hua <jim870718@gmail.com>
2021-05-12 18:34:06 +0800

    update rect.html

--- script/script.js
+++
@@ -1,9 +0,0 @@
-import { createNode } from './svg.js'
-
-var canvas = document.getElementById('main-canvas')
-
-canvas.addEventListener('click', function (e) {
-    var canvasBoundingRect = canvas.getBoundingClientRect()
-    var node = createNode(e.pageX - canvasBoundingRect.left, e.pageY - canvasBoundingRect.top)
-    canvas.appendChild(node)
-})
--- script/toolbar.js
+++ script/toolbar.js
@@ -1,11 +1,12 @@
-// var toolBarButtonList = document.querySelectorAll('.button-list div')
+var toolBarButtonList = document.querySelectorAll('.button-list div')
+var toolValue = 0
 
-
-// toolBarButtonList.forEach(function (element) {
-//     element.addEventListener('click', () => {
-//         if (!element.classList.contains('selected')) {
-//             document.querySelector('.button-list .selected').classList.remove('selected')
-//             element.classList.add('selected')
-//         }
-//     })
-// })
+toolBarButtonList.forEach(function (element) {
+    element.addEventListener('click', () => {
+        if (!element.classList.contains('selected')) {
+            document.querySelector('.button-list .selected').classList.remove('selected')
+            element.classList.add('selected')
+            toolValue = +element.id
+        }
+    })
+})
