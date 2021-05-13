3c9d0052fa0c1517f3a7a3fbf70213f0ee33ea33
Li Chun Hua <jim870718@gmail.com>
2021-05-13 12:40:59 +0800

    .

--- script/toolbar.js
+++ script/toolbar.js
@@ -1,9 +1,12 @@
-var toolBarButtonList = document.querySelectorAll('.button-list div')
+var toolBarButtonList = document.querySelectorAll('.button-list > div')
 var toolValue = 0
 
 toolBarButtonList.forEach(function (element) {
     element.addEventListener('click', () => {
-        if (!element.classList.contains('selected')) {
+        if (element === document.querySelector('.erease')) {
+            document.querySelector('#main-canvas').innerHTML = ''
+        }
+        else if (!element.classList.contains('selected')) {
             document.querySelector('.button-list .selected').classList.remove('selected')
             element.classList.add('selected')
             toolValue = +element.id
